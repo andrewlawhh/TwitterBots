dc504ca4005e38b0041d2a6ee152a144e913839a
Andrew Law <andrewlaw8@gmail.com>
2018-06-10 15:37:58 -0700

    added exception handling (IncompleteRead Exception)

--- gaming_news_updater.py
+++ gaming_news_updater.py
@@ -6,6 +6,7 @@ from credentials import *
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
+api.wait_on_rate_limit = True
 
 
 # Helper function to get user ID by username
@@ -13,24 +14,15 @@ def get_id(user_name):
     return str(api.get_user(screen_name = user_name).id)
 
 def get_name(id):
-    return api.get_user(id).screen_name
+    return api.get_user(id = id).screen_name
 
-followed_accounts = [get_id('E3'),
-                            get_id('Sony'),
-                            get_id('Bethesda'),
-                            get_id('BethesdaStudios'),
-                            get_id('Microsoft'),
-                            get_id('CDPROJEKTRED'),
-                            get_id('Activision'),
-                            get_id('BandaiNamcoUS'),
-                            get_id('CapcomUSA_'),
-                            get_id('Dell'),
-                            get_id('EpicGames'),
-                            get_id('nvidia'),
-                            get_id('Ubisoft'),
-                            get_id('EA'),
-                            get_id('Naughty_Dog')
-                            ]
+account_names = ['E3', 'Sony', 'Bethesda',' BethesdaStudios', 'Microsoft',
+                 'CDPROJEKTRED', 'Activision', 'BandaiNamcoUS', 'CapcomUSA_',
+                 'Dell', 'EpicGames', 'nvidia', 'Ubisoft', 'EA', 'Naughty_Dog',
+                 'anthemgame', 'Fallout', 'gameinformer', 'IGN', 'Nintendo',
+                 'Treyarch']
+
+followed_accounts = [get_id(name) for name in account_names]
 
 # Set up stream to retweet
 class GameAccListener(tweepy.StreamListener):
@@ -39,21 +31,22 @@ class GameAccListener(tweepy.StreamListener):
         try:
             print('tweet by:', get_name(status.user.id))
             print('user id:', status.user.id)
-            print('tweet text :', status.text)
-
-            # followed accounts list contains strings
-            if str(status.user.id) in followed_accounts:
-                print('retweeted tweet by', get_name(status.user.id))
+            print('tweet text:', status.text)
+            if status.text.startswith('RT @') or get_name(status.user.id) in account_names:
+                print('RETWEET')
                 api.retweet(status.id)
-
-            # loop every 15 seconds
-            time.sleep(15)
+            time.sleep(5)
+            # loop every 5 seconds
         except Exception as e:
             print(e)
         return True
 
     def on_error(self, status_code):
-        print("error : ", str(status_code))
+        print("error occurred")
+        return True
+
+    def on_exception(self, exception):
+        print("exception occured", exception)
         return True
 
     def on_timeout(self):
